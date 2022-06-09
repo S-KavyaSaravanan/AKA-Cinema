aae0845a41001f3f39ee262a9796feb81ee5d685
S-KavyaSaravanan <95293366+S-KavyaSaravanan@users.noreply.github.com>
2022-06-09 13:44:42 +0530

    Update AuthenticationService.java

--- src/main/java/com/aka/cinema/service/AuthenticationService.java
+++ src/main/java/com/aka/cinema/service/AuthenticationService.java
@@ -1,1 +1,24 @@
+package com.aka.cinema.service;
+
+import java.util.Base64.Encoder;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lhm.secondhandstore.model.StudentEntity;
+import com.lhm.secondhandstore.repository.StudentRepository;
+
+@Service
+public class AuthenticationService {
+	
+   
+	public boolean isValidUser(String email, String passWord) {
+		
+			return true;
+		}
+		
+	}
+
+	
 
