6256f2348e246726b3e418964742eac1a179df5b
S-KavyaSaravanan <95293366+S-KavyaSaravanan@users.noreply.github.com>
2022-06-09 13:33:00 +0530

    Update AuthenticationApi.java

--- src/main/java/Api/AuthenticationApi.java
+++ src/main/java/Api/AuthenticationApi.java
@@ -1,1 +1,26 @@
+package com.aka.cinema.Api;
 
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public interface AutenticationApi {
+	
+	
+	  @RequestMapping(value = "/login", consumes="application/json", produces = {
+	  "application/json" }, method = RequestMethod.POST) ResponseEntity<?>
+	  login(@RequestBody String loginDetails);
+	  
+	  @RequestMapping(value = "/register", consumes="application/json", produces =
+	  { "application/json" }, method = RequestMethod.POST) ResponseEntity<?>
+	  register(@RequestBody String registerdetails);
+	  
+	  @RequestMapping(value = "/logout", consumes="application/json", produces = {
+	  "application/json" }, method = RequestMethod.GET) ResponseEntity<?> logOut();
+	 
+	 
+}
